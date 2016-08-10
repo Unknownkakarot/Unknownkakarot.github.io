f2dac29401c824c648fb9d0665402f19a7b7da4a
Unknownkakarot <jfyjfy23@gmail.com>
2016-08-10 15:35:03 +0800

    publish game for summary

---
+++ game/js/index.js
@@ -0,0 +1,47 @@
+function game() {
+
+var question=prompt("Who is Stronger? Goku or Superman?")
+if (question === "goku") {
+  alert("Right, superman sucks");
+}
+
+else { 
+  alert("HAHAHAHAHAH THAT'S FUNNY!");
+}
+
+var question2=prompt("Wait a minute, are you sure?");
+if (question2 === "yes") {
+  alert("YOU ARE A TRUE DB FAN!!!");
+}
+
+else { 
+  alert("lol nah bro");
+}
+
+var question3=prompt("DC fans come to you, what do you do? CAll ANIME FRIENDS and beat them up or be a friend with them");
+if (question3 === "beat them up") {
+  alert("YOU WON THE WAR!!!")
+}
+
+else { alert("Good job, you have played yourself...");}
+
+var question4=prompt("Superman is trying to kill goku, what do you do? Give him energy or let him die?");
+if (question4 === "Give him energy") {
+  alert("GOKU WON EASILY BECAUSE OF YOUR HELP! GOOD JOB!");
+}
+
+else { 
+  alert("Goku won anyways lol");
+}
+
+var question5=prompt("Buy a dozen of kryptonite or none?");
+if (question5 === "dozen") {
+  alert("superman is gone");
+}
+
+else { 
+  alert("hey where is he? oh he's ded anyways.");
+}
+  
+alert("thanks for playing")  
+}
